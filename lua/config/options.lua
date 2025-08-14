b323f281b9acb4dd6c1dccda7c18670870e440a7
iam-prabha <vel.prabhakaran.ds@gmail.com>
2025-08-14 19:13:03 +0530

    terminal toggle features added

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Disable LazyVim plugin order checking
+vim.g.lazyvim_check_order = false
